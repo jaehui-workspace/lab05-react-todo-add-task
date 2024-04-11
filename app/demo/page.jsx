6c7bdf33b544308b67cfa7d95cd140c7a4840ad7
jaehui-workspace <kor_232@hotmail.com>
2024-04-11 13:23:45 -0600

    fixed

--- app/demo/page.jsx
+++ app/demo/page.jsx
@@ -18,11 +18,15 @@ async function DemoPage({children}) {
 				<h2 className="text-5xl text-slate-900">Demo Page Component </h2>
 			</header>
 			<main className=" max-w-md mx-auto  py-24">
-				<ToDoList className=" space-y-4 p-4 h-[478px] max-h-[478px] overflow-y-scroll shadow-sm rounded-md border border-neutral-200">
-					{tasks.map((task) => (
-						<ToDoListItem key={task[0]} />
-					))}
-				</ToDoList>
+			<ToDoList className="space-y-4 p-4 h-[478px] max-h-[478px] overflow-y-scroll shadow-sm rounded-md border border-neutral-200">
+                    {tasks.map((task) => (
+                        <ToDoListItem
+                            key={task[0]}
+                            category={task[1].category}
+                            todo={task[1].todo}
+                        />
+                    ))}
+                </ToDoList>
 				<AddTask />
 			</main>
 		</>
